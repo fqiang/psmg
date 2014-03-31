3f44bf36e00f260f271d5ee5abef984d72e9eda1
f.qiang@gmail.com <f.qiang@gmail.com@4057e72c-88d5-72e0-da89-92cbe0633c9d>
2014-03-31 01:52:03 +0000

    remove the log file

--- trunk/psmg-oops/Release/src/context/subdir.mk
+++ trunk/psmg-oops/Release/src/context/subdir.mk
@@ -5,36 +5,60 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../src/context/Block.cpp \
-../src/context/CBlock.cpp \
-../src/context/EMBlock.cpp \
+../src/context/BlockCons.cpp \
+../src/context/BlockObj.cpp \
 ../src/context/ExpandedModel.cpp \
 ../src/context/IndexSet.cpp \
 ../src/context/ModelContext.cpp \
+../src/context/PValue.cpp \
+../src/context/PValueSym.cpp \
+../src/context/PValueValue.cpp \
 ../src/context/Param.cpp \
+../src/context/ParamMult.cpp \
+../src/context/ParamSingle.cpp \
 ../src/context/Set.cpp \
-../src/context/Var.cpp 
+../src/context/SetOrdered.cpp \
+../src/context/SetSimple.cpp \
+../src/context/Var.cpp \
+../src/context/VarSingle.cpp 
 
 OBJS += \
 ./src/context/Block.o \
-./src/context/CBlock.o \
-./src/context/EMBlock.o \
+./src/context/BlockCons.o \
+./src/context/BlockObj.o \
 ./src/context/ExpandedModel.o \
 ./src/context/IndexSet.o \
 ./src/context/ModelContext.o \
+./src/context/PValue.o \
+./src/context/PValueSym.o \
+./src/context/PValueValue.o \
 ./src/context/Param.o \
+./src/context/ParamMult.o \
+./src/context/ParamSingle.o \
 ./src/context/Set.o \
-./src/context/Var.o 
+./src/context/SetOrdered.o \
+./src/context/SetSimple.o \
+./src/context/Var.o \
+./src/context/VarSingle.o 
 
 CPP_DEPS += \
 ./src/context/Block.d \
-./src/context/CBlock.d \
-./src/context/EMBlock.d \
+./src/context/BlockCons.d \
+./src/context/BlockObj.d \
 ./src/context/ExpandedModel.d \
 ./src/context/IndexSet.d \
 ./src/context/ModelContext.d \
+./src/context/PValue.d \
+./src/context/PValueSym.d \
+./src/context/PValueValue.d \
 ./src/context/Param.d \
+./src/context/ParamMult.d \
+./src/context/ParamSingle.d \
 ./src/context/Set.d \
-./src/context/Var.d 
+./src/context/SetOrdered.d \
+./src/context/SetSimple.d \
+./src/context/Var.d \
+./src/context/VarSingle.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
